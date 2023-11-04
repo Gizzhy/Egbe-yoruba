7ff4869a33ced1fde0a3d1543d7a146634d775f8
Gizzhy <gizzhyakinz@gmail.com>
2023-11-04 23:54:55 +0100

    hero added

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,7 +1,30 @@
 import "../../styles/hero.css";
+// import heroimg from "../../assets/images/hero-img.svg";
 
 const Hero = () => {
-  return <></>;
+  return (
+    <>
+      <div className="hero-main">
+        <div className="hero2">
+          <p className="hero-w">
+            We Are Proud Of Our
+            <br />
+            Cultural Heritage
+          </p>
+          <p className="hero-w2">
+            Association Of Yoruba Progressive Community is a community of yoruba
+            immigrants in bayern germany which aims to bring yoruba people
+            around germany together and unite them through events.
+          </p>
+          <p className="hero-w3"> discover yoruba culture</p>
+
+          <div className="hero-bb">
+            <button className="mis-button">Join Us</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
