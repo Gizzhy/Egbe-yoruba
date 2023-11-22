34cafe1bb6a78c428a4c32456e54c6a8080a2996
Gizzhy <gizzhyakinz@gmail.com>
2023-11-22 10:48:30 +0100

    gallery section

--- src/pages/home/Gallery.jsx
+++ src/pages/home/Gallery.jsx
@@ -1,14 +1,84 @@
 import "../../styles/gallery.css";
+import React, { useState } from "react";
 // import yoruba1 from "../../assets/images/image 3.svg";
 
 // import React, { useState, useEffect } from "react";
 
 const Gallery = () => {
+  const [activeButton, setActiveButton] = useState("button1");
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
   return (
     <>
       <div className="gallery">
         <div className="gallery1">
-          <p className="gallery-w">Proverbs</p>
+          <p className="gallery-w">Gallery</p>
+        </div>
+        <p className="gallery-w2">
+          check out our gallery from recent concluded events.
+        </p>
+        <div className="gallery-things">
+          <div className="gallery-things1">
+            <div className="gallery-things2">
+              <div className="gallery-things2-1">
+                <div className="gallery-things3">
+                  <button
+                    style={{
+                      backgroundColor:
+                        activeButton === "button1" ? "#F28C0F" : "",
+                      color: activeButton === "button1" ? "#090B0E" : "",
+                      fontWeight:
+                        activeButton === "button1" ? "bold" : "normal",
+                      borderRadius: activeButton === "button1" ? "6px" : "",
+                    }}
+                    className="gallery-button"
+                    onClick={() => handleButtonClick("button1")}
+                  >
+                    All
+                  </button>
+                  <button
+                    style={{
+                      backgroundColor:
+                        activeButton === "button2" ? "#F28C0F" : "",
+                      color: activeButton === "button2" ? "#090B0E" : "",
+                      fontWeight:
+                        activeButton === "button2" ? "bold" : "normal",
+                      borderRadius: activeButton === "button2" ? "6px" : "",
+                    }}
+                    className="gallery-button"
+                    onClick={() => handleButtonClick("button2")}
+                  >
+                    Images
+                  </button>
+                  <button
+                    style={{
+                      backgroundColor:
+                        activeButton === "button3" ? "#F28C0F" : "",
+                      color: activeButton === "button3" ? "#090B0E" : "",
+                      fontWeight: activeButton === "button3" ? "600" : "normal",
+                      borderRadius: activeButton === "button3" ? "6px" : "",
+                    }}
+                    className="gallery-button"
+                    onClick={() => handleButtonClick("button3")}
+                  >
+                    Videos
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {activeButton === "button1" && (
+              <div className="gallery-area">All</div>
+            )}
+            {activeButton === "button2" && (
+              <div className="gallery-area">Images</div>
+            )}
+            {activeButton === "button3" && (
+              <div className="gallery-area">Videos</div>
+            )}
+          </div>
         </div>
       </div>
     </>
