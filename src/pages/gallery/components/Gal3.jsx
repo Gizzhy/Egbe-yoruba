e6d26598a64a446fb9cbc475742a615699f43fad
Gizzhy <gizzhyakinz@gmail.com>
2024-02-10 18:10:00 +0100

    reponsive

--- src/pages/gallery/components/Gal3.jsx
+++ src/pages/gallery/components/Gal3.jsx
@@ -1,5 +1,82 @@
+// images
+import eiog1 from "../assets/eiog/1eiog.jpg";
+import eiog2 from "../assets/eiog/2eiog.jpg";
+import eiog3 from "../assets/eiog/3eiog.jpg";
+import eiog4 from "../assets/eiog/4eiog.jpg";
+import eiog5 from "../assets/eiog/5eiog.jpg";
+import eiog6 from "../assets/eiog/6eiog.jpg";
+import eiog7 from "../assets/eiog/7eiog.jpg";
+import eiog8 from "../assets/eiog/8eiog.jpg";
+import eiog9 from "../assets/eiog/9eiog.jpg";
+import eiog10 from "../assets/eiog/10eiog.jpg";
+import eiog11 from "../assets/eiog/11eiog.jpg";
+import eiog12 from "../assets/eiog/12eiog.jpg";
+// images
+
+// component
+import Gal3sub from "./Gal3sub";
+// component
+
+// styles
+import "./gal3.css";
+// styles
+
 const Gal3 = () => {
-  return <></>;
+  return (
+    <>
+      <Gal3sub />
+
+      <div className="pic-first">
+        <div className="picoo">
+          <img src={eiog1} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog2} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog3} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog4} alt="germany2022" className="pic-real" />
+        </div>
+      </div>
+      {/* next */}
+      {/* next */}
+      {/* next */}
+
+      <div className="pic-first">
+        <div className="picoo">
+          <img src={eiog5} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog6} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog7} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog8} alt="germany2022" className="pic-real" />
+        </div>
+      </div>
+      {/* next */}
+      {/* next */}
+      {/* next */}
+      <div className="pic-first">
+        <div className="picoo">
+          <img src={eiog9} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog10} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog11} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog12} alt="germany2022" className="pic-real" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Gal3;
---
+++ src/pages/gallery/components/Gal3sub.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+// import { Carousel } from "antd";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+// images
+import eiog13 from "../assets/eiog/13eiog.jpg";
+import eiog14 from "../assets/eiog/14eiog.jpg";
+import eiog15 from "../assets/eiog/15eiog.jpg";
+import eiog16 from "../assets/eiog/16eiog.jpg";
+import eiog17 from "../assets/eiog/17eiog.jpg";
+import eiog18 from "../assets/eiog/18eiog.jpg";
+import eiog19 from "../assets/eiog/19eiog.jpg";
+import eiog20 from "../assets/eiog/20eiog.jpg";
+// import eiog21 from "../assets/eiog/21eiog.jpg";
+// import eiog22 from "../assets/eiog/22eiog.jpg";
+// import eiog23 from "../assets/eiog/23eiog.jpg";
+// import eiog24 from "../assets/eiog/24eiog.jpg";
+// images
+
+// import InfiniteCarousel from "react-leaf-carousel";
+
+const Gal3sub = () => {
+  //
+  return (
+    <>
+      <Carousel
+        additionalTransfrom={0}
+        arrows={false}
+        autoPlay
+        autoPlaySpeed={1800}
+        centerMode={false}
+        className=""
+        containerClass="container-with-dots"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite={true}
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        pauseOnHover
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024,
+            },
+            items: 3,
+            partialVisibilityGutter: 40,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 1,
+            partialVisibilityGutter: 30,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464,
+            },
+            items: 2,
+            partialVisibilityGutter: 30,
+          },
+        }}
+        rewind={false}
+        rewindWithAnimation={false}
+        rtl={false}
+        shouldResetAutoplay
+        showDots={false}
+        sliderClass=""
+        slidesToSlide={2}
+        swipeable
+      >
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog14} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog15} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog16} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog17} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog18} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog19} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog20} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+        <div className="picoo">
+          <img src={eiog13} alt="germany2022" className="pic-real" />
+        </div>
+      </Carousel>
+    </>
+  );
+};
+
+export default Gal3sub;
