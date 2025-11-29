8af2601c3aeb0c9e137a7068222555d8f1dd4f1c
Gizzhy <gizzhyakinz@gmail.com>
2025-11-29 10:26:25 +0100

    president repositioning

--- src/pages/about/Executive.jsx
+++ src/pages/about/Executive.jsx
@@ -36,21 +36,31 @@ const Executive = () => {
           <div className="execu1">
             <div className="execu1-1">
               <div className="execuimg">
-                <img src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/alfred_t9mtyj.jpg" className="execuimgg2" />
-                <p className="execu-oro">MR ALFRED FATAI</p>
-                <p className="execu-oro2">{"General Secretary"}</p>
-              </div>
-            </div>
-            <div className="execu1-2">
-              <div className="execuimg">
-                <img src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/akinola_rgg8rw.jpg" className="execuimgg" />
+                <img
+                  src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/akinola_rgg8rw.jpg"
+                  className="execuimgg"
+                />
                 <p className="execu-oro">MR AKINOLA FAMSON</p>
                 <p className="execu-oro2">{"President"}</p>
               </div>
             </div>
+            <div className="execu1-2">
+              <div className="execuimg">
+                <img
+                  src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/alfred_t9mtyj.jpg"
+                  className="execuimgg2"
+                />
+                <p className="execu-oro">MR ALFRED FATAI</p>
+                <p className="execu-oro2">{"General Secretary"}</p>
+              </div>
+            </div>
+
             <div className="execu1-3">
               <div className="execuimg">
-                <img src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/adetula_qrztg9.jpg" className="execuimgg" />
+                <img
+                  src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/adetula_qrztg9.jpg"
+                  className="execuimgg"
+                />
                 <p className="execu-oro">Chief Wilson Adetula</p>
                 <p className="execu-oro2">{"Vice president"}</p>
               </div>
@@ -60,14 +70,20 @@ const Executive = () => {
           <div className="execu11">
             <div className="execu1-1">
               <div className="execuimg">
-                <img src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192694/lekan_g5skur.jpg" className="execuimgg2" />
+                <img
+                  src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192694/lekan_g5skur.jpg"
+                  className="execuimgg2"
+                />
                 <p className="execu-oro">MR OLALEKAN ELEGBEDE</p>
                 <p className="execu-oro2">{"Financial Secretary"}</p>
               </div>
             </div>
             <div className="execu1-3">
               <div className="execuimg">
-                <img src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/kayode_vrirwa.jpg" className="execuimgg2" />
+                <img
+                  src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/kayode_vrirwa.jpg"
+                  className="execuimgg2"
+                />
                 <p className="execu-oro">MR KAYODE OLASOJI</p>
                 <p className="execu-oro2">{"Assistant Gen. Secretary"}</p>
               </div>
@@ -77,21 +93,30 @@ const Executive = () => {
           <div className="execu2">
             <div className="execu1-1">
               <div className="execuimg">
-                <img src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/ghazal_hpglkx.jpg" className="execuimgg" />
+                <img
+                  src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/ghazal_hpglkx.jpg"
+                  className="execuimgg"
+                />
                 <p className="execu-oro">MR GHAZAL BABALOLA</p>
                 <p className="execu-oro2">{"Social Secretary"}</p>
               </div>
             </div>
             <div className="execu1-2">
               <div className="execuimg">
-                <img src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/bashir_yisrlz.jpg" className="execuimgg" />
+                <img
+                  src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192693/bashir_yisrlz.jpg"
+                  className="execuimgg"
+                />
                 <p className="execu-oro">MR Muhammed Balogun</p>
                 <p className="execu-oro2">{"Treasurer"}</p>
               </div>
             </div>
             <div className="execu1-3">
-            <div className="execuimg">
-                <img src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192462/olufemi_ttaheg.jpg" className="execuimgg" />
+              <div className="execuimg">
+                <img
+                  src="https://res.cloudinary.com/ducgskep8/image/upload/v1732192462/olufemi_ttaheg.jpg"
+                  className="execuimgg"
+                />
                 <p className="execu-oro">MR OLUFEMI OKANLAWON</p>
                 <p className="execu-oro2">{"Publicity Secretary"}</p>
               </div>
